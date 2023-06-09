89eef81231f7c68ea9541dcb524a3b9f7d87ae71
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-06-09 15:37:16 -0500

    Refractor:Code no longer removes from wrong list or double spawns

--- Assets/Scripts/IslandSpawning.cs
+++ Assets/Scripts/IslandSpawning.cs
@@ -6,12 +6,17 @@ public class IslandSpawning : MonoBehaviour
 {
     float gridSize = 10f;
     [SerializeField] List<GameObject> islandList = new List<GameObject>();
+    List<GameObject> inGameIsland = new List<GameObject>();
     Grid worldGrid;
 
 
     private void Awake()
     {
         worldGrid = gameObject.GetComponent<Grid>();
+        for (int i = 0; i < islandList.Count; i++)
+        {
+            inGameIsland.Add(islandList[i]);
+        }
     }
     // Start is called before the first frame update
     void Start()
@@ -29,17 +34,17 @@ public class IslandSpawning : MonoBehaviour
     {
         for (int i = 0; i < islandList.Count; i++)
         {
-            int randIsland = Random.Range(0, islandList.Count);
-            Debug.Log("Spawning: " + islandList[randIsland].name + " at position: " + i);
+            int randIsland = Random.Range(0, inGameIsland.Count);
+            Debug.Log("Spawning: " + inGameIsland[randIsland].name + " at position: " + i);
 
             //We can move around this randomness based off how many islands etc
             float x = Mathf.Round(Random.Range(-25, 25) / gridSize) * gridSize;
             float y = Mathf.Round(Random.Range(-25, 25) / gridSize) * gridSize;
             Vector3 worldPos = worldGrid.GetCellCenterWorld(new Vector3Int((int)x, (int)y, 0));
 
-            Instantiate(islandList[randIsland], worldPos, Quaternion.identity);
+            Instantiate(inGameIsland[randIsland], worldPos, Quaternion.identity);
 
-            islandList.Remove(islandList[randIsland]);
+            inGameIsland.Remove(inGameIsland[randIsland]);
         }
     }
 }
