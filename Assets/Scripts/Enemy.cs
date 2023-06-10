ab2ba92f849b75aa33437c0dd624ce2e03ae4bfc
sustachio <sustachio@gmail.com>
2023-06-10 14:18:27 -0500

    Fix: create enemy list at start

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public static List<Vector3> enemyList;
+    public static List<Vector3> enemyList = new List<Vector3>();
     private int enemyId;
 
     public float health = 50;
