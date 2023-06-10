adbd8103edf88ef1373f4933f0d069b9dc32f460
sustachio <sustachio@gmail.com>
2023-06-10 18:35:13 -0500

    Fix: comment out unused knockback distance

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,7 +11,7 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] float movementSpeed = 1f;
     [SerializeField] float attackDistance = 2f;
-    [SerializeField] float knockbackDistance = 1f;
+    // [SerializeField] float knockbackDistance = 1f;
 
     private Sword sword;
 
