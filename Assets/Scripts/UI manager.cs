5baf30f2ef3b65236f76610c41ec7f3903ec5f04
sustachio <sustachio@gmail.com>
2023-06-10 15:55:50 -0500

    Fix: remove old healthbar

--- Assets/Scripts/UI manager.cs
+++ Assets/Scripts/UI manager.cs
@@ -6,7 +6,6 @@ using TMPro;
 
 public class UImanager : GenericSingleton<UImanager>
 {
-    [SerializeField] Image healthBar;
     [SerializeField] TMP_Text resourceText;
     RectTransform healthBar;
 
