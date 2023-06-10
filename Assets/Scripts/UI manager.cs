249f2f1a7d2e9a1beb6d87dc9c09f6d031226201
sustachio <sustachio@gmail.com>
2023-06-10 15:51:22 -0500

    UI Fix:Giving to Luke

--- Assets/Scripts/UI manager.cs
+++ Assets/Scripts/UI manager.cs
@@ -6,16 +6,19 @@ using TMPro;
 
 public class UImanager : GenericSingleton<UImanager>
 {
-    [SerializeField] Image healthBar;
-    [SerializeField] TMP_Text resourceText;
+    [SerializeField] RectTransform healthBar;
 
-    public void DisplayResources(float wood, float stone) {
-
+    private void Start()
+    {
+        healthBar = GameObject.Find("HealthBarInner").GetComponent<RectTransform>();
     }
+
+    public void DisplayResources(float wood, float stone) { }
     public void DisplayPlayerHealthBar(float health)
     {
-        healthBar.fillAmount = Mathf.Clamp01(health / 100f);
-    } // out of 100
+        healthBar.localScale = new Vector3(230.2151f * (health / 100f), 0.675f, 1f);
+        healthBar.anchoredPosition = new Vector3(162.72185f + 230.2151f * (health / 200f), healthBar.anchoredPosition.y, 0);
+    } 
     public void DisplayEnemyHealth(List<Vector3> enemies) { } // vector 3 being (x, y, health) in world cords
 
     private void Update()
