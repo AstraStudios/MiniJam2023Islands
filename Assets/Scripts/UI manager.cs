2b8f817d9ce4a696f8fbf675b156f127f6d374c0
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-06-10 14:33:00 -0500

    Fixes??

--- Assets/Scripts/UI manager.cs
+++ Assets/Scripts/UI manager.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UImanager : GenericSingleton<UImanager>
 {
+    [SerializeField] Image healthBar;
+
     public void DisplayResources(float wood, float stone) { }
-    public void DisplayPlayerHealthBar(float health) { } // out of 100
-    public void DisplayEnemyHealth(List<Vector3> enimes) { } // vector 3 being (x, y, health) in world cords
+    public void DisplayPlayerHealthBar(float health) {
+        healthBar.fillAmount = Mathf.Clamp01(health / 100);
+    } // out of 100
+    public void DisplayEnemyHealth(List<Vector3> enemies) { } // vector 3 being (x, y, health) in world cords
 
     private void Update()
     {
