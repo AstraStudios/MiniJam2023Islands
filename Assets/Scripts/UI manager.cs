b3d11d85a0f2df9e8ddd49ae8696d2f86232d383
sustachio <sustachio@gmail.com>
2023-06-10 12:25:21 -0500

    Feature start: ui functions that need to be completed

---
+++ Assets/Scripts/UI manager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UImanager : MonoBehaviour
+{
+    void DisplayResources(float wood, float stone) { }
+    void DisplayPlayerHealthBar(float health) { } // out of 100
+    void DisplayEnemyHealth(Vector3[] enimes) { } // vector 3 being (x, y, health) in world cords
+}
