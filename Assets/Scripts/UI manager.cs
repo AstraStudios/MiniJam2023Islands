916b2c96d430c8e6616e1dae08ccf0cfbf31e55d
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-06-10 14:50:23 -0500

    New textures

--- Assets/Scripts/UI manager.cs
+++ Assets/Scripts/UI manager.cs
@@ -9,8 +9,9 @@ public class UImanager : GenericSingleton<UImanager>
     [SerializeField] Image healthBar;
 
     public void DisplayResources(float wood, float stone) { }
-    public void DisplayPlayerHealthBar(float health) {
-        healthBar.fillAmount = Mathf.Clamp01(health / 100);
+    public void DisplayPlayerHealthBar(float health)
+    {
+        healthBar.fillAmount = Mathf.Clamp01(health / 100f);
     } // out of 100
     public void DisplayEnemyHealth(List<Vector3> enemies) { } // vector 3 being (x, y, health) in world cords
 
